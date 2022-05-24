dbcd1c4699913f461e9ebbb26244f9b55a50ccae
dweller4085 <immbirj@gmail.com>
2022-05-24 20:27:26 +0700

    hashtable carcass

--- hash/hash.cpp
+++ hash/hash.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 #include <string>
 
-class i_hashtable {
-  virtual void add (t_keyval &) = 0;
-  virtual void remove (t_keyval &) = 0;
-  virtual bool check (t_keyval &) = 0;
-};
-
 struct t_keyval {
-  std::size_t K;
+  unsigned K;
   std::string str;
 };
 
-std::size_t hash_v1 (std::size_t K, std::size_t N, std::size_t i = 1) {
-	if (i == 0) return 0;
-  return (hash_v1 (K, N, i - 1) + 4 * i + 4 * i * i) % N;
-}
+struct i_hash_function {
+	virtual unsigned operator () (unsigned, unsigned, unsigned) = 0;
+};
 
-class t_hashtable_v2 : public i_hashtable {
+struct t_hash_v1 : public i_hash_function {
+	unsigned operator () (unsigned K, unsigned N, unsigned i) override {
+		if (i == 0) return K % N;
+		return (operator () (K, N, i - 1) + 4 * i + 4 * i * i) % N;
+	}
+} v1;
+
+class t_hashtable {
 private:
-	struct t_kvnode {
-		t_keyval kv;
-		t_kvnode * next;
-	} * table;
-  std::size_t size;
-  std::size_t (*hash_function) (std::size_t, std::size_t, std::size_t) = hash_v1;
-
+  struct t_kvnode {
+    t_keyval kv;
+    t_kvnode * next;
+  } * table;
+  unsigned size;
+  i_hash_function * hash_function = &v1;
 public:
+  void add (t_keyval &);
+  void remove (t_keyval &);
+  bool check (t_keyval &);
 };
 
 int main () {}
