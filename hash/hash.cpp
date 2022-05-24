9dd09a59e69f9671f0fe9f5e3f2e90837bbf689a
dweller4085 <immbirj@gmail.com>
2022-05-24 12:43:00 +0700

    hashtable implementation example

---
+++ hash/hash.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+class i_hashtable {
+  virtual void add () = 0;
+  virtual void remove () = 0;
+  virtual bool check () = 0;
+};
+
+struct t_keyval {
+  std::size_t K;
+  std::string str;
+};
+
+std::size_t hash_v1 (std::size_t K, std::size_t N, std::size_t i = 1) {
+  return (0 + 4 * i + 4 * i * i) % N;
+}
+
+class t_hashtable_v2 : public i_hashtable {
+private:
+	struct t_kvnode {
+		t_keyval kv;
+		t_kvnode * next;
+	} * table;
+  std::size_t size;
+  std::size_t (*hash_function) (std::size_t, std::size_t, std::size_t) = hash_v1;
+
+public:
+};
+
+int main () {}
